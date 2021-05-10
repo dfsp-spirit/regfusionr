92d9a2c109f6422d7b500dc1c80457c285866674
Tim Schäfer <ts+code@rcmd.org>
2021-05-10 17:19:38 +0200

    add test for MNI152 using ANTs reg

--- tests/testthat/test-vol_to_fsaverage.R
+++ tests/testthat/test-vol_to_fsaverage.R
@@ -4,7 +4,7 @@ get_test_file <- function(filename) {
   system.file("extdata", "testdata", filename, package = "regfusionr", mustWork = TRUE);
 }
 
-test_that("The colin27 data m3z transformation works", {
+test_that("The Colin27 data projection using m3z registration works", {
   # Load expected result data.
   expected_res_file_lh = get_test_file('lh.Colin.RF_M3Z_Colin27_norm_to_fsaverage.nii.gz');
   expected_res_file_rh = get_test_file('rh.Colin.RF_M3Z_Colin27_norm_to_fsaverage.nii.gz');
@@ -38,4 +38,38 @@ test_that("The colin27 data m3z transformation works", {
   # fsbrain::vis.symmetric.data.on.subject("~/software/freesurfer/subjects", "fsaverage", morph_data_lh = actual_lh, morph_data_rh = actual_rh, surface = "inflated");
 })
 
+
+test_that("The MNI152 data projection using ANTs registration works", {
+  # Load expected result data.
+  expected_res_file_lh = get_test_file('lh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
+  expected_res_file_rh = get_test_file('rh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
+  expected_lh = freesurferformats::read.fs.volume(expected_res_file_lh);
+  expected_rh = freesurferformats::read.fs.volume(expected_res_file_rh);
+  testthat::expect_true(is.array(expected_lh));
+  testthat::expect_true(is.array(expected_rh));
+  testthat::expect_equal(length(as.vector(expected_lh)), 163842);
+  testthat::expect_equal(length(as.vector(expected_rh)), 163842);
+
+  # Load input image for test.
+  input_img = get_test_file('MNI_probMap_ants.central_sulc.nii.gz');
+  out_dir = tempdir();
+  outfiles = vol_to_fsaverage(input_img, out_dir, template_type='MNI152_orig', rf_type='RF_ANTs', out_type = 'curv');
+
+  # Compare results with expected result data.
+  testthat::expect_true(file.exists(outfiles$lh));
+  testthat::expect_true(file.exists(outfiles$rh));
+  actual_lh = freesurferformats::read.fs.morph(outfiles$lh);
+  actual_rh = freesurferformats::read.fs.morph(outfiles$rh);
+  testthat::expect_true(is.vector(actual_lh));
+  testthat::expect_true(is.vector(actual_rh));
+  testthat::expect_equal(length(actual_lh), 163842);
+  testthat::expect_equal(length(actual_rh), 163842);
+
+  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = 1e-5);
+  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = 1e-5);
+
+  #fsbrain::vis.data.on.fsaverage(morph_data_lh = actual_lh, morph_data_rh = actual_rh);
+  #
+  # fsbrain::vis.symmetric.data.on.subject("~/software/freesurfer/subjects", "fsaverage", morph_data_lh = actual_lh, morph_data_rh = actual_rh, surface = "inflated");
+})
 

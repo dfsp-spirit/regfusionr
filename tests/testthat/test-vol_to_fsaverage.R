abefdb840da84eca51965616d4f99744da913a94
Tim Schäfer <ts+code@rcmd.org>
2021-05-10 16:54:19 +0200

    fix test

--- tests/testthat/test-vol_to_fsaverage.R
+++ tests/testthat/test-vol_to_fsaverage.R
@@ -12,11 +12,13 @@ test_that("The colin27 data m3z transformation works", {
   expected_rh = freesurferformats::read.fs.volume(expected_res_file_rh);
   testthat::expect_true(is.array(expected_lh));
   testthat::expect_true(is.array(expected_rh));
+  testthat::expect_equal(length(as.vector(expected_lh)), 163842);
+  testthat::expect_equal(length(as.vector(expected_rh)), 163842);
 
   # Load input image for test.
   input_img = get_test_file('Colin_probMap_ants.central_sulc.nii.gz');
   out_dir = tempdir();
-  outfiles = vol_to_fsaverage(input_img, out_dir, template_type='Colin27_norm', rf_type='RF_M3Z', out_type = 'curv')
+  outfiles = vol_to_fsaverage(input_img, out_dir, template_type='Colin27_norm', rf_type='RF_M3Z', out_type = 'curv');
 
   # Compare results with expected result data.
   testthat::expect_true(file.exists(outfiles$lh));
@@ -25,9 +27,11 @@ test_that("The colin27 data m3z transformation works", {
   actual_rh = freesurferformats::read.fs.morph(outfiles$rh);
   testthat::expect_true(is.vector(actual_lh));
   testthat::expect_true(is.vector(actual_rh));
+  testthat::expect_equal(length(actual_lh), 163842);
+  testthat::expect_equal(length(actual_rh), 163842);
 
-  testthat::expect_equal(actual_lh, expected_lh);
-  testthat::expect_equal(actual_rh, expected_rh);
+  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = 1e-5);
+  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = 1e-5);
 })
 
 

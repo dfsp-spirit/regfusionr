7f98782231d8a0e5eff36985ba68d41d22e3d32e
Tim Schäfer <ts+code@rcmd.org>
2021-05-11 11:37:20 +0200

    do not check dir if NULL, add test with direct data return

--- tests/testthat/test-vol_to_fsaverage.R
+++ tests/testthat/test-vol_to_fsaverage.R
@@ -4,6 +4,11 @@ get_test_file <- function(filename) {
   system.file("extdata", "testdata", filename, package = "regfusionr", mustWork = TRUE);
 }
 
+#' Get float comparison tolerance.
+tol <- function() {
+  1e-2
+}
+
 test_that("The Colin27 data projection using m3z registration works", {
   # Load expected result data.
   expected_res_file_lh = get_test_file('lh.Colin.RF_M3Z_Colin27_norm_to_fsaverage.nii.gz');
@@ -30,8 +35,8 @@ test_that("The Colin27 data projection using m3z registration works", {
   testthat::expect_equal(length(actual_lh), 163842);
   testthat::expect_equal(length(actual_rh), 163842);
 
-  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = 1e-5);
-  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = 1e-5);
+  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = tol());
+  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = tol());
 
   #fsbrain::vis.data.on.fsaverage(morph_data_lh = actual_lh, morph_data_rh = actual_rh);
   #
@@ -39,7 +44,7 @@ test_that("The Colin27 data projection using m3z registration works", {
 })
 
 
-test_that("The MNI152 data projection using ANTs registration works", {
+test_that("The MNI152 data projection using ANTs registration works using output curv files", {
   # Load expected result data.
   expected_res_file_lh = get_test_file('lh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
   expected_res_file_rh = get_test_file('rh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
@@ -53,7 +58,7 @@ test_that("The MNI152 data projection using ANTs registration works", {
   # Load input image for test.
   input_img = get_test_file('MNI_probMap_ants.central_sulc.nii.gz');
   out_dir = tempdir();
-  outfiles = vol_to_fsaverage(input_img, out_dir, template_type='MNI152_orig', rf_type='RF_ANTs', out_type = 'curv');
+  outfiles = vol_to_fsaverage(input_img, template_type='MNI152_orig', rf_type='RF_ANTs', out_type = 'curv', out_dir = out_dir);
 
   # Compare results with expected result data.
   testthat::expect_true(file.exists(outfiles$lh));
@@ -65,11 +70,47 @@ test_that("The MNI152 data projection using ANTs registration works", {
   testthat::expect_equal(length(actual_lh), 163842);
   testthat::expect_equal(length(actual_rh), 163842);
 
-  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = 1e-5);
-  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = 1e-5);
+  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = tol());
+  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = tol());
 
   #fsbrain::vis.data.on.fsaverage(morph_data_lh = actual_lh, morph_data_rh = actual_rh);
   #
   # fsbrain::vis.symmetric.data.on.subject("~/software/freesurfer/subjects", "fsaverage", morph_data_lh = actual_lh, morph_data_rh = actual_rh, surface = "inflated");
 })
+
+
+test_that("The MNI152 data projection using ANTs registration works returning data", {
+  # Load expected result data.
+  expected_res_file_lh = get_test_file('lh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
+  expected_res_file_rh = get_test_file('rh.MNI.RF_ANTs_MNI152_orig_to_fsaverage.nii.gz');
+  expected_lh = freesurferformats::read.fs.volume(expected_res_file_lh);
+  expected_rh = freesurferformats::read.fs.volume(expected_res_file_rh);
+  testthat::expect_true(is.array(expected_lh));
+  testthat::expect_true(is.array(expected_rh));
+  testthat::expect_equal(length(as.vector(expected_lh)), 163842);
+  testthat::expect_equal(length(as.vector(expected_rh)), 163842);
+
+  # Load input image for test.
+  input_img = get_test_file('MNI_probMap_ants.central_sulc.nii.gz');
+  out_dir = NULL;
+  outdata = vol_to_fsaverage(input_img, template_type='MNI152_orig', rf_type='RF_ANTs', out_type = 'curv', out_dir = out_dir);
+
+  actual_lh = outdata$lh;
+  actual_rh = outdata$rh;
+  testthat::expect_true(is.vector(actual_lh));
+  testthat::expect_true(is.vector(actual_rh));
+  testthat::expect_equal(length(actual_lh), 163842);
+  testthat::expect_equal(length(actual_rh), 163842);
+
+  testthat::expect_equal(actual_lh, as.vector(expected_lh), tolerance = tol());
+  testthat::expect_equal(actual_rh, as.vector(expected_rh), tolerance = tol());
+
+  #fsbrain::vis.data.on.fsaverage(morph_data_lh = actual_lh, morph_data_rh = actual_rh);
+  #
+  # fsbrain::vis.symmetric.data.on.subject("~/software/freesurfer/subjects", "fsaverage", morph_data_lh = actual_lh, morph_data_rh = actual_rh, surface = "inflated");
+  #
+  # plot differences expected - actual:
+  #fsbrain::vis.symmetric.data.on.subject("~/software/freesurfer/subjects", "fsaverage", morph_data_lh = (expected_lh - actual_lh), morph_data_rh = (expected_rh - actual_rh), surface = "inflated");
+})
+
 

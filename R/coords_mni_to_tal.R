719c9c38fc7ae38685608a0d8d4eb3355ae696ee
Tim Schäfer <ts+code@rcmd.org>
2021-11-03 17:51:48 +0100

    add almost empty talairach file

--- R/coords_mni_to_tal.R
+++ R/coords_mni_to_tal.R
@@ -1,2 +1,2 @@
 # see https://brainmap.org/training/BrettTransform.html
-# and https://github.com/sccn/dipfit/blob/master/mni2tal.m
+# and https://github.com/sccn/dipfit/blob/master/mni2tal.m and https://github.com/sccn/dipfit/blob/master/mni2tal_matrix.m

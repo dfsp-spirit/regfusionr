cd6f4302d84f94c97c735aa750eb9c44e271f7e7
Tim Schäfer <ts+code@rcmd.org>
2021-11-18 12:41:31 +0100

    add new test for mni305 to mni152 by coords

--- tests/testthat/test-fsaverage_to_coords.R
+++ tests/testthat/test-fsaverage_to_coords.R
@@ -1,7 +1,7 @@
 
 
 
-test_that("MNI152 coords can be mapped to fsaverage space", {
+test_that("fsaverage vertices can be mapped to MNI152 space", {
 
   # The next line is a setup for Tim's test system only and should be removed once this package is official.
   Sys.setenv("FS_HOME"=file.path(Sys.getenv("HOME"), "software/freesurfer/"));
@@ -21,3 +21,31 @@ test_that("MNI152 coords can be mapped to fsaverage space", {
   testthat::expect_true(is.matrix(mni_coord));
   testthat::expect_equal(length(mni_coord), 3L);
 })
+
+
+test_that("MNI305 coordinates can be mapped to MNI152 space", {
+
+  # The next line is a setup for Tim's test system only and should be removed once this package is official.
+  Sys.setenv("FS_HOME"=file.path(Sys.getenv("HOME"), "software/freesurfer/"));
+
+  if(nchar(Sys.getenv("FS_HOME")) == 0L) {
+    testthat::skip("No FreeSurfer installation found or FS_HOME environment variable not set correctly.");
+  }
+  if(! dir.exists(Sys.getenv("FS_HOME"))) {
+    testthat::skip("No FreeSurfer installation found at path given in FS_HOME environment variable.");
+  }
+
+  fsaverage_home = file.path(Sys.getenv("FS_HOME"), "subjects");
+  query_fsaverage_vertex = c(9092L);
+  query_hemis = c("rh");
+  rh_orig_surf = freesurferformats::read.fs.surface(file.path(fsaverage_home, "fsaverage", "surf", "rh.orig"));
+  query_vertex_coords = rh_orig_surf$vertices[query_fsaverage_vertex, ];
+
+  mni_coord = mni305_coords_to_mni152_coords(query_vertex_coords);
+
+  testthat::expect_true(is.matrix(mni_coord));
+  testthat::expect_equal(length(mni_coord), 3L);
+})
+
+
+

8c76e4455354ae00977f472a274834304540cf81
Tim Schäfer <ts+code@rcmd.org>
2022-03-30 14:18:04 +0200

    implement mask

--- R/fsaverage_to_vol.R
+++ R/fsaverage_to_vol.R
@@ -134,8 +134,8 @@ fsaverage_to_vol <- function(lh_input, rh_input, template_type="MNI152_orig", rf
       rh_coord = freesurferformats::read.fs.mgh(rh_map_file, with_header = FALSE, drop_empty_dims = TRUE); # 3x16777216 matrix
 
       # TODO: create lh_mask and rh_mask
-      lh_mask = ; # 1x16777216 matrix (or vector if dropped), logical
-      rh_mask = ; # 1x16777216 matrix (or vector if dropped), logical
+      lh_mask = which(colSums(lh_coord) != 0L); # 1x16777216 matrix (or vector if dropped), logical
+      rh_mask = which(colSums(rh_coord) != 0L); # 1x16777216 matrix (or vector if dropped), logical
 
       out = list();
 
